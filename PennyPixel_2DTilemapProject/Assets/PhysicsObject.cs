ce820040f3860217a6003717e2c4dec7753aebc9
inewhart <inewhart@iu.edu>
2020-02-24 20:04:33 -0500

    almost done with video 4

--- PennyPixel_2DTilemapProject/Assets/PhysicsObject.cs
+++ PennyPixel_2DTilemapProject/Assets/PhysicsObject.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class PhysicsObject : MonoBehaviour
 {
     protected Vector2 velocity;
+    public float minimumGroundNormalY = .65f;
     public float gravityModifier = 1f;
     protected Rigidbody2D rb2d;
+    protected bool grounded;
+    protected Vector2 groundNormal;
     protected const float minMoveDistance = 0.001f;
     protected ContactFilter2D contactFilter;
     protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D> (16);
+    
     protected const float shellRadius = 0.01f;
     void OnEnable()
     {
@@ -31,16 +36,35 @@ public class PhysicsObject : MonoBehaviour
     void FixedUpdate()
     {
         velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+        grounded = false;
         Vector2 deltaPosition = velocity * Time.deltaTime;
         Vector2 move = Vector2.up * deltaPosition.y;
-        Movement(move);
+        Movement(move,true);
     }
-    void Movement(Vector2 move)
+    void Movement(Vector2 move,bool yMovement)
     {
         float distance = move.magnitude;
         if(distance > minMoveDistance)
         {
             int count = rb2d.Cast(move,contactFilter, hitBuffer, distance + shellRadius);
+            for(int i = 0; i <count; i++)
+            {
+                hitBufferList.Add(hitBuffer[i]);
+            }
+            for(int i = 0; i < hitBufferList.Count; i++)
+            {
+                Vector2 currentNormal = hitBufferList[i].normal;
+                if(currentNormal.y > minimumGroundNormalY)
+                {
+                    grounded = true;
+                    if(yMovement)
+                    {
+                        groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+                float projection = Vector2.Dot(velocity,currentNormal);
+            }
         }
         rb2d.position = rb2d.position + move;
     }
