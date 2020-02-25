c5f3568a3b7f4d4956b84b99c9d7a3eb8296cd25
inewhart <inewhart@iu.edu>
2020-02-24 20:23:17 -0500

    Finished next video

--- PennyPixel_2DTilemapProject/Assets/PhysicsObject.cs
+++ PennyPixel_2DTilemapProject/Assets/PhysicsObject.cs
@@ -7,6 +7,7 @@ public class PhysicsObject : MonoBehaviour
     protected Vector2 velocity;
     public float minimumGroundNormalY = .65f;
     public float gravityModifier = 1f;
+    protected Vector2 targetVelocity;
     protected Rigidbody2D rb2d;
     protected bool grounded;
     protected Vector2 groundNormal;
@@ -36,9 +37,13 @@ public class PhysicsObject : MonoBehaviour
     void FixedUpdate()
     {
         velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+        velocity.x = targetVelocity.x;
         grounded = false;
         Vector2 deltaPosition = velocity * Time.deltaTime;
-        Vector2 move = Vector2.up * deltaPosition.y;
+        Vector2 moveAlongGround = new Vector2(groundNormal.y,-groundNormal.x);
+        Vector2 move = moveAlongGround * deltaPosition.x;
+        Movement(move,false);
+        move = Vector2.up * deltaPosition.y;
         Movement(move,true);
     }
     void Movement(Vector2 move,bool yMovement)
@@ -64,8 +69,14 @@ public class PhysicsObject : MonoBehaviour
                     }
                 }
                 float projection = Vector2.Dot(velocity,currentNormal);
+                if(projection < 0)
+                {
+                    velocity = velocity - projection * currentNormal;
+                }
+                float modifiedDist = hitBufferList[i].distance - shellRadius;
+                distance = modifiedDist < distance ? modifiedDist : distance;
             }
         }
-        rb2d.position = rb2d.position + move;
+        rb2d.position = rb2d.position + move.normalized * distance;
     }
 }
