c537853643db445baa1f0ab62f700f444133401d
inewhart <inewhart@iu.edu>
2020-03-03 17:13:52 -0500

    End of lab commit

---
+++ PennyPixel_2DTilemapProject/Assets/Scripts/Wingame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Wingame : MonoBehaviour
+{
+    public GameObject chest;
+    public Text action;
+    // Start is called before the first frame update
+    void Start()
+    {
+        action.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var Distance = chest.transform.position.x - this.transform.position.x;
+        if(Distance <= .5f)
+        {
+            action.text = "Press E To Open";
+        }
+        if(Input.GetButtonDown("e"))
+        {
+            // SceneManager.LoadScene
+        }
+    }
+}
